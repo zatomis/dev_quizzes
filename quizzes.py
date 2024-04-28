528daecdb774fcfded4311f43f0a82abc31a5674
zatomis <zatomis@list.ru>
2024-04-28 11:56:21 +0300

    Бот

--- quizzes.py
+++ quizzes.py
@@ -1,5 +1,14 @@
+import logging
+from environs import Env
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import os
 import random
+
+
+logger = logging.getLogger(__name__)
+question = dict()
+answer = dict()
 
 
 def load_quizzes():
@@ -20,7 +29,36 @@ def load_quizzes():
             number += 1
     return question, answer
 
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Здравствуйте {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    )
+    env: Env = Env()
+    env.read_env()
+    token_bot = env('BOT_TOKEN')
+    updater = Updater(token_bot)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    logger.setLevel(logging.INFO)
+    logger.info('Бот Игра - викторина')
     question, answer = load_quizzes()
-    print(question)
-    print(answer)
+
+    while True:
+        updater.start_polling()
+        updater.idle()
