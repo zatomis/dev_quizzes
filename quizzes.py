acde778d2eac6822f5474b991d30830f2e0ed8e6
zatomis <zatomis@list.ru>
2024-04-30 13:56:51 +0300

    Redis в качестве словаря

--- quizzes.py
+++ quizzes.py
@@ -5,18 +5,17 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import os
 import random
+import redis
 
-
+redis_answer = redis.Redis()
+redis_question = redis.Redis()
+question_answer_count = 0
 logger = logging.getLogger(__name__)
-question = dict()
-answer = dict()
 
 
 def load_quizzes():
     quizzes = []
     directory = 'questions'
-    question = dict()
-    answer = dict()
     number = 0
     for file in os.listdir(directory):
         if file.endswith(".txt"):
@@ -25,17 +24,16 @@ def load_quizzes():
     quizzes = random.choice(quizzes).split('\n\n')
     for i, question_answer in enumerate(quizzes):
         if "Вопрос" in question_answer:
-            question[number] = question_answer
-            answer[number] = quizzes[i+1]
+            redis_question.set(number, question_answer)
+            redis_answer.set(number,quizzes[i+1])
             number += 1
-    return question, answer
+    return number
 
 
 def start(update: Update, context: CallbackContext) -> None:
     custom_keyboard = [['Новый вопрос', 'Сдаться'],
                        ['Мой счет']]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Здравствуйте {user.mention_markdown_v2()}\!',
@@ -45,8 +43,8 @@ def start(update: Update, context: CallbackContext) -> None:
 
 def echo(update: Update, context: CallbackContext) -> None:
     if update.message.text == 'Новый вопрос':
-        question_now = question[random.randint(0,len(question))]
-        update.message.reply_text(question_now)
+        question_now = redis_question.get(random.randint(0, question_answer_count))
+        update.message.reply_text(question_now.decode("utf-8"))
 
 
 if __name__ == '__main__':
@@ -63,6 +61,7 @@ if __name__ == '__main__':
 
     logger.setLevel(logging.INFO)
     logger.info('Бот Игра - викторина')
-    question, answer = load_quizzes()
+    question_answer_count = load_quizzes()
+    print(question_answer_count)
     updater.start_polling()
     updater.idle()
